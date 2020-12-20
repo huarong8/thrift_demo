296fab9686ce5cf01c7c495d6a756ddff3d3aed1
zhanghaibo <haiboz8@163.com>
2020-12-21 00:51:36 +0800

    update

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.15
 
 require (
 	github.com/apache/thrift v0.13.0
+	github.com/gin-gonic/gin v1.6.3
 )
--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"flag"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+func ginServe(){
+	r := gin.Default()
+	r.POST("/v1/item", func(context *gin.Context) {
+		rsp := Client()
+		if rsp != nil {
+			context.JSON(http.StatusOK, rsp)
+		}else{
+			context.JSON(http.StatusInternalServerError, "")
+		}
+	})
+	r.Run("localhost:8881")
+}
 
 func main(){
 	var f string
-	flag.StringVar(&f, "flag", "client", "client or server ?")
+	flag.StringVar(&f, "f", "client", "client or server ?")
 	flag.Parse()
 
 	if f == "client"{
-		Client()
+		ginServe()
 	}else{
 		Serve()
 	}
